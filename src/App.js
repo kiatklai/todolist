20d95b0afb2a889afb60d2d4f99466d680f29435
kiatklai <likhitthaworn.k@gmail.com>
2023-08-09 11:35:27 +0900

    CSSでデザイン変更

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import './App.css';
 function App() {
   return (
     <section className='container'>
       <h1>ToDoList</h1>
       <form className='form-group'>
-        <input type='text' className='text-input'/>
-        <button type='submit' className='submit-btn'>Add</button>
+        <div className="form-control">
+          <input type='text' className='text-input'/>
+          <button type='submit' className='submit-btn'>Add</button>
+        </div>
       </form>
     </section>
   );
